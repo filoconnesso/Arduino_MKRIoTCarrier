19c1b4ec55919f912e58445d3dd0ba0919dd5c87
Ubi de Feo <me@ubidefeo.com>
2021-02-20 15:01:25 +0100

    Fix RGB LEDs colour order
    The current colour arrangement used for DotStar has Green and Red swapped.
    At least on my unit this is what I experience.
    This PR fixes the order to `DOTSTAR_BGR`, but it might be useful to check if different batches of APA102c ordered for production might have different colour order

--- src/Arduino_MKRIoTCarrier.h
+++ src/Arduino_MKRIoTCarrier.h
@@ -119,7 +119,7 @@ class MKRIoTCarrier{
     Adafruit_ST7789 display = Adafruit_ST7789(&SPI, TFT_CS, TFT_DC, -1);
 	
     //RGB LEDs
-    Adafruit_DotStar leds = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+    Adafruit_DotStar leds = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
     private:
 };
 #endif
